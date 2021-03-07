dc2f901473e9caf12d4fa29f7e35b87cc872e012
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-03-07 20:42:51 +0530

    Update sample_config.py

--- Sibyl_System/sample_config.py
+++ Sibyl_System/sample_config.py
@@ -1,9 +1,11 @@
 from typing import Union, Optional
 
-API_ID: int = 12345
-API_HASH: str = "XYZ"
-STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Sibyl_logs: Union[str, int] = 123456789
-Sibyl_approved_logs: Union[str, int] = 123456789
+API_ID: int = 2147833
+API_HASH: str = "75aaffe84071e6ceb7ab4e07b4a4a266"
+STRING_SESSION: str = "1BVtsOLUBu2GXIsLKUGTZkSgwO0AEeBcEPWmsPCPFZyZnstFNn1ltw64jD2h4MqMQxHGh_8NeJuZx5h0fW5gMg8s-4IBlz6Vw2Ks_-ttOKn7jPzdL70MbxKAlCfFwh3r_4WNIasUkineZxi7IL4qu5mmQjVWBdJYwjH-TauiLgd13MWrAy9XqT0kqyP2uG7Cw0BGAGyKKFXOzhiLUg-raH7wwcDWxxMZKhSrRtJXJT9Ga3f0A9yx2OCIhAdHZUk9t2WbgWFnJfVBpqKUullUCBy18VVuxes60an-0nFMr9jD7fZ4buWUQTKqabYeJO6xcgkSVFvtVMXLe45oR53zqGrC1d8a7x-c="
+Sibyl_logs: Union[str, int] = -1001293004442
+Sibyl_approved_logs: Union[str, int] = -1001274776877
 GBAN_MSG_LOGS: Optional[Union[str, int]] = None
-BOT_TOKEN: str = "123456:shsiajskzkxhxj"
+BOT_TOKEN: str = "1615719817:AAHqQZWGvFBXVW1lTnnkv4216aYs1wNuafk"
+INSPECTORS = data["1634756943"]
+DEVELOPERS = data["1418166549"]
